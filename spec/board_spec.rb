df46bb4c0e1796e91eb5827246f3995736d4ac47
Brandon Newton <sandalsboy@gmail.com>
2015-01-29 20:32:01 -0800

    Empty Board
    Ascii board.

---
+++ spec/board_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+require "board"
+
+RSpec.describe ConnectFour::Board do
+	it "displays itself as ascii art" do
+		board = ConnectFour::Board.new
+		ascii = board.display
+
+		blank_board = <<-EOF
+[][][][][][][]
+[][][][][][][]
+[][][][][][][]
+[][][][][][][]
+[][][][][][][]
+[][][][][][][]
+		EOF
+
+		expect(ascii).to eq(blank_board)
+	end
+end
+
